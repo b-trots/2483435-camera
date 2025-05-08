4688a74e449b16dfe3b0099e0a10dc66f87c8fa2
BVTrots <bvtrots@gmail.com>
2025-05-08 12:11:13 +0300

    feat(basketIcon): Добавили иконку корзину в хедер
    Добавлено:
    - Иконка корзины в хедере;
    - Отображение текущего количества товаров в корзине

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,29 @@
-import { BemClass } from '@/const/const';
+import { BemClass, DefaultParam } from '@/const/const';
 import { Logo } from './logo';
 import { HeaderNav } from './header-nav';
 import { memo } from 'react';
 import { Search } from './search/search';
+import { BasketIcon } from '../basket-icon';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '@/const/const-navigate';
+import { useAppSelector } from '@/hooks/hooks';
+import { getTotalQuantity } from '@/store/slices/order/order-selectors';
 
 function HeaderComponent() {
+  const totalQuantity = useAppSelector(getTotalQuantity);
+
   return (
     <header className="header" id="header">
       <div className="container">
         <Logo bemBlock={BemClass.Header} />
         <HeaderNav />
         <Search />
+        <Link className="header__basket-link" to={AppRoute.Basket}>
+          <BasketIcon />
+          {totalQuantity !== DefaultParam.ZeroValue && (
+            <span className="header__basket-count">{totalQuantity}</span>
+          )}
+        </Link>
       </div>
     </header>
   );
--- src/store/slices/order/order-selectors.ts
+++ src/store/slices/order/order-selectors.ts
@@ -1,10 +1,10 @@
-import { RequestStatus, SliceName } from '@/const/const';
+import { DefaultParam, RequestStatus, SliceName } from '@/const/const';
 import { State } from '@/types/store-types/store-types';
+import { createSelector } from '@reduxjs/toolkit';
 
 type OrderState = Pick<State, SliceName.Order>;
 
 const getCoupon = (state: OrderState) => state[SliceName.Order].coupon;
-
 const getBasket = (state: OrderState) => state[SliceName.Order].basket;
 
 const getOrderRequestStatus = (state: OrderState): RequestStatus =>
@@ -12,4 +12,14 @@ const getOrderRequestStatus = (state: OrderState): RequestStatus =>
 
 const getOrderError = (state: OrderState) => state[SliceName.Order].orderError;
 
-export { getCoupon, getBasket, getOrderRequestStatus, getOrderError };
+const getTotalQuantity = createSelector([getBasket], (basket) =>
+  basket.reduce((acc, camera) => acc + camera.quantity, DefaultParam.ZeroValue)
+);
+
+export {
+  getCoupon,
+  getBasket,
+  getOrderRequestStatus,
+  getOrderError,
+  getTotalQuantity,
+};
